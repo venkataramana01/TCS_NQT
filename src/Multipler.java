0c18ecb603ac4209da12ca3c34e38e62695e9e0e
venkataramana01 <venkataramana.achuta@sasi.ac.in>
2026-07-10 16:12:22 +0530

    Multipler

---
+++ src/Multipler.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Multipler {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        int m=scan.nextInt();
+        System.out.println(m%n==0?"Exafct Multipler":"Not Exact");
+    }
+}
